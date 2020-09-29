7cc1ae119047c84dfd5207e23f42fea793310b6c
AssanSav <assane.savadogo81@gmail.com>
2020-09-29 13:14:43 -0400

    JS stack

---
+++ src/basics/JsStack.js
@@ -0,0 +1,27 @@
+const _items = new WeakMap()
+
+class Stack {
+    constructor() {
+        _items.set(this, [])
+    }
+
+    push(obj) {
+        return _items.get(this).push(obj)
+    }
+
+    pop(){
+        let items = _items.get(this)
+        if(items.length === 0) throw new Error("Stack is empty")
+        return items.pop()
+    }
+
+    peek(){
+        let items = _items.get(this)
+        if(items.length === 0) throw new Error("Stack is empty")
+        return items[items.length-1]  
+    }
+
+    count(){
+        return _items.get(this).length
+    }
+}
